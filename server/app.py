1619386d8ecf28740cc61e6ce7b53bf46c4c6ab9
Asma Shahin <asmashahin@Asmas-MacBook-Air.local>
2026-04-01 15:29:06 +0530

    add default user for login

--- server/app.py
+++ server/app.py
@@ -76,8 +76,22 @@ def test():
 def home():
     return {"message": "Academic Progress Tracker Backend Running"}
 # create tables
+# create tables + default user
 with app.app_context():
     db.create_all()
+
+    from models.models import User
+    from werkzeug.security import generate_password_hash
+
+    # 🔥 Create default user if not exists
+    if not User.query.filter_by(email="admin@test.com").first():
+        user = User(
+            name="Admin",
+            email="admin@test.com",
+            password=generate_password_hash("123456")
+        )
+        db.session.add(user)
+        db.session.commit()
 if __name__ == '__main__':  
     app.run()
 
