4444171ffe04af2edfec1548b44bacd1305b849f
Asma Shahin <asmashahin@Asmas-MacBook-Air.local>
2026-04-01 14:57:53 +0530

    update API url for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://academic-backend-vo9i.onrender.com",
 });
 
 // Attach JWT token automatically
